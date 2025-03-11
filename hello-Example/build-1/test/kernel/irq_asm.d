8f1c541ce142f485391e6de8c0a0b7156b93ef57
Amr Badra <amrbadra@sas.upenn.edu>
2025-03-11 03:25:14 +0200

    Hello CV32

---
+++ hello-Example/build-1/test/kernel/irq_asm.d
@@ -0,0 +1,8 @@
+/home/abadra/GP/elf_gen_ibex/pulp-rt-examples/hello/build/test/kernel/irq_asm.o: \
+ /home/abadra/GP/elf_gen_ibex/pulpissimo/sw/pulp-runtime/kernel/irq_asm.S \
+ /home/abadra/GP/elf_gen_ibex/pulpissimo/sw/pulp-runtime/include/chips/pulpissimo/config.h \
+ /home/abadra/GP/elf_gen_ibex/pulpissimo/sw/pulp-runtime/include/archi/pulp_defs.h
+
+/home/abadra/GP/elf_gen_ibex/pulpissimo/sw/pulp-runtime/include/chips/pulpissimo/config.h:
+
+/home/abadra/GP/elf_gen_ibex/pulpissimo/sw/pulp-runtime/include/archi/pulp_defs.h:
